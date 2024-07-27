4f3373501e00c5da7e16c6066818051da3bbbe9d
AndresRoches <maroches@eaglehubs.com>
2024-07-27 00:39:24 -0600

    entity and lint configs

--- src/database/app-data-source.ts
+++ src/database/app-data-source.ts
@@ -1,7 +1,7 @@
-import { DataSource } from "typeorm"
+import { DataSource, DataSourceOptions } from "typeorm"
 import { config } from "../config/local"
 export const myDataSource = new DataSource(
     {
         ...config.db
-    }
+    } as DataSourceOptions
 )
