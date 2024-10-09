01667f5c7e0a97878c9484ce88224f192cb34686
Zucely Hernández <65913550+anzucel@users.noreply.github.com>
2024-10-09 00:22:59 -0600

    feat(query.routes): adding guet route to list all guests

--- src/config/local.ts
+++ src/config/local.ts
@@ -1,17 +1,40 @@
 import { DataSourceOptions } from "typeorm";
 import { Entities } from "../entities/entities.array";
+import { User } from "../entities/user.entity";
+import { Empresa } from "../entities/empresa.entity";
+import { Evento } from "../entities/evento.entity";
+import { GastosEvento } from "../entities/gastoEvento.entity";
+import { Invitacion } from "../entities/invitacion.entity";
+import { Patrocinador } from "../entities/patrocinador.entity";
+import { Patrocinio } from "../entities/patrocinio.entity";
+import { Persona } from "../entities/persona.entity";
+import { Proveedor } from "../entities/proveedor.entity";
+import { ProveedoresEvento } from "../entities/proveedoresEvento.entity";
+import { Segmento } from "../entities/segmento.entity";
 
 
 const databaseConfig: DataSourceOptions =
 {
     type: "mysql",
-    host: "host",
+    host: "meetup-database.cxewu6gc4gul.us-east-2.rds.amazonaws.com",
     port: 3306,
     username: "username",
     password: "password",
     database: "MeetUpDev",
     // agregar las entidades a la configuración
-    entities: Entities,
+    entities: [
+        User,
+        Empresa,
+        Evento,
+        GastosEvento,
+        Invitacion,
+        Patrocinador,
+        Patrocinio,
+        Persona,
+        Proveedor,
+        ProveedoresEvento,
+        Segmento
+    ],
     logging: true,
     synchronize: true,
 }
--- src/routes/query.routes.ts
+++ src/routes/query.routes.ts
@@ -1,10 +1,12 @@
 // src/routes/queryRoutes.ts
 import { Router } from "express";
 import { getAllUsers, getUserById } from "../controllers/query.controller";
+import { getAllGuest } from "../queries/get-all-guest.query";
 
 const router = Router();
 
 router.get("/users", getAllUsers);
 router.get("/users/:id", getUserById);
 
+router.get("/guest", getAllGuest);
 export default router;
