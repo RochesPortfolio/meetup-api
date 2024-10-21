179c57db686c54e876101ae3b8128a1b84183585
Marlon Roches <144393832+AndresRoches@users.noreply.github.com>
2024-10-21 13:33:11 -0600

    Update send-invite.command.ts

--- src/commands/send-invite.command.ts
+++ src/commands/send-invite.command.ts
@@ -2,7 +2,31 @@ import { Request, Response } from "express";
 import { SendMailProps, SendMailService } from "../services/send-mail.service";
 import loggerService from "../services/logger.service";
 import { HttpStatus } from "../enums/http-code.enum";
-
+/**
+ * @swagger
+ * /send-mail:
+ *   post:
+ *     summary: Enviar uno o varios correos electrónicos
+ *     tags: [Correo]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               emails:
+ *                 oneOf:
+ *                   - $ref: '#/components/schemas/SendMailProps'
+ *                   - type: array
+ *                     items:
+ *                       $ref: '#/components/schemas/SendMailProps'
+ *     responses:
+ *       200:
+ *         description: Correo(s) enviado(s) exitosamente
+ *       500:
+ *         description: Error enviando correo(s)
+ */
 export const sendMailCommand = async (req: Request, res: Response) => {
     const emails: SendMailProps | SendMailProps[] = req.body;
     try {
