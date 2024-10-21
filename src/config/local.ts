273269a90b9f0257e972332946e1ff9ce7b2eab7
Marlon Roches <144393832+AndresRoches@users.noreply.github.com>
2024-10-21 13:49:56 -0600

    Update local.ts

--- src/config/local.ts
+++ src/config/local.ts
@@ -1,47 +1,36 @@
 import { DataSourceOptions } from "typeorm";
 import { Entities } from "../entities/entities.array";
-import { User } from "../entities/user.entity";
-import { Empresa } from "../entities/empresa.entity";
-import { Evento } from "../entities/evento.entity";
-import { GastosEvento } from "../entities/gastoEvento.entity";
-import { Invitacion } from "../entities/invitacion.entity";
-import { Patrocinador } from "../entities/patrocinador.entity";
-import { Patrocinio } from "../entities/patrocinio.entity";
-import { Persona } from "../entities/persona.entity";
-import { Proveedor } from "../entities/proveedor.entity";
-import { ProveedoresEvento } from "../entities/proveedoresEvento.entity";
-import { Segmento } from "../entities/segmento.entity";
-
 
 const databaseConfig: DataSourceOptions =
 {
     type: "mysql",
-    host: "meetup-database.cxewu6gc4gul.us-east-2.rds.amazonaws.com",
     port: 3306,
-    username: "username",
-    password: "password",
+    host: "meetup-database.cxewu6gc4gul.us-east-2.rds.amazonaws.com",
+    username: "admin",
+    password: "rootRDS123!",
     database: "MeetUpDev",
-    // agregar las entidades a la configuración
-    entities: [
-        User,
-        Empresa,
-        Evento,
-        GastosEvento,
-        Invitacion,
-        Patrocinador,
-        Patrocinio,
-        Persona,
-        Proveedor,
-        ProveedoresEvento,
-        Segmento
-    ],
+    entities: Entities,
     logging: true,
     synchronize: true,
 }
 
+const accountTransport = {
+    service: "gmail",
+    auth: {
+        type: "OAuth2",
+        user: "meetupdev427@gmail.com",
+        clientId: "clientId",
+        clientSecret: "clientSecret",
+        refreshToken: "refreshToken",
+        accessToken:""
+    }
+};
+
 export const config = {
     server: {
         port: 3030,
+        // baseUrl: "http://localhost:3000",
     },
-    db: databaseConfig
+    db: databaseConfig,
+    mailing: accountTransport,
 };
