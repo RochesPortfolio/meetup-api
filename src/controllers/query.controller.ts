feb51b37fc18aa00da2167a2e8a57b32655d80d5
Marlon Roches <144393832+AndresRoches@users.noreply.github.com>
2024-10-22 00:07:20 -0600

    controller and entity added

--- src/controllers/query.controller.ts
+++ src/controllers/query.controller.ts
@@ -1,6 +1,7 @@
 import { getAllInvites } from "../queries/get-all-guest.query";
 import { getAllUsers } from "../queries/get-all-users.query";
+import { getEnterpricesWithPersons } from "../queries/get-empresas-with-personas.query";
 import { getInviteInfo } from "../queries/get-invite-info.query";
 import { getUserById } from "../queries/get-user-by-id.query";
 
-export { getAllUsers, getUserById,getInviteInfo,getAllInvites };
+export { getAllUsers, getUserById,getInviteInfo,getAllInvites, getEnterpricesWithPersons };
