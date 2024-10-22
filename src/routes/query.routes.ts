ed10142c89d6792c2a12cde8e417616c667a8f97
Marlon Roches <144393832+AndresRoches@users.noreply.github.com>
2024-10-22 00:09:56 -0600

    add query and service

--- src/routes/query.routes.ts
+++ src/routes/query.routes.ts
@@ -1,6 +1,6 @@
 // src/routes/queryRoutes.ts
 import { Router } from "express";
-import { getAllInvites, getAllUsers, getInviteInfo, getUserById } from "../controllers/query.controller";
+import { getAllInvites, getAllUsers, getEnterpricesWithPersons, getInviteInfo, getUserById } from "../controllers/query.controller";
 
 const router = Router();
 
@@ -8,6 +8,9 @@ router.get("/users", getAllUsers);
 router.get("/users/:id", getUserById);
 router.get("/invites", getAllInvites);
 router.get("/invite/info/:hash_invite", getInviteInfo);
+
+router.get("/getEnterprisesWithPersons", getEnterpricesWithPersons);
+router.get("/enterprisesWithPersons", getEnterpricesWithPersons);
 
 
 export default router;
