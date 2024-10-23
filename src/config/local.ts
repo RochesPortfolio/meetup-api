fe8e9656ab0427acc85131f108da82f7027a0e3f
Marlon Roches <144393832+AndresRoches@users.noreply.github.com>
2024-10-23 03:22:44 -0600

    local remove keys

--- src/config/local.ts
+++ src/config/local.ts
@@ -1,28 +1,29 @@
 import { DataSourceOptions } from "typeorm";
 import { Entities } from "../entities/entities.array";
+import * as dotenv from 'dotenv';
 
-const databaseConfig: DataSourceOptions =
-{
-    type: "mysql",
-    port: 3306,
-    host: "meetup-database.cxewu6gc4gul.us-east-2.rds.amazonaws.com",
-    username: "admin",
-    password: "rootRDS123!",
-    database: "MeetUpDev",
+dotenv.config();
+const databaseConfig: DataSourceOptions = {
+    type: process.env.DB_TYPE as any,
+    port: parseInt(process.env.DB_PORT || '3306'),
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     entities: Entities,
     logging: true,
-    synchronize: false,
-}
+    synchronize: true,
+};
 
 const accountTransport = {
-    service: "gmail",
+    service: process.env.GMAIL_SERVICE,
     auth: {
-        type: "OAuth2",
-        user: "meetupdev427@gmail.com",
-        clientId: "clientId",
-        clientSecret: "clientSecret",
-        refreshToken: "refreshToken",
-        accessToken:""
+        type: process.env.GMAIL_AUTH_TYPE,
+        user: process.env.GMAIL_USER,
+        clientId: process.env.GMAIL_CLIENT_ID,
+        clientSecret: process.env.GMAIL_CLIENT_SECRET,
+        refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+        accessToken: process.env.GMAIL_ACCESS_TOKEN,
     }
 };
 
